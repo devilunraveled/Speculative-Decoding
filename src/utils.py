02721818277b385d078c71cfac56d656b8e07d58
Hardik Sharma <hardiksharmah73@gmail.com>
2024-10-07 19:10:33 +0530

    modified decoder architecture

---
+++ src/utils.py
@@ -0,0 +1,20 @@
+from numpy import ndarray as NDArray
+import random
+
+############ SAMPLING TECHNIQUES ############
+
+def getMostProbableToken( distribution : NDArray ) :
+    """
+    Returns the token index with the highest probabilty. 
+    """
+    return distribution.argmax(axis = -1)
+
+def getATokenFromTopK( distribution : NDArray, k : int ) :
+    """
+    Returns a weighted random choice from the normalized
+    probability distribution from the top k most 
+    probable tokens.
+    """
+    topKIndices = distribution.argsort(axis=-1)[-k:]
+    topKValues = distribution[topKIndices]
+    return random.choices(population=topKIndices, weights=topKValues)
