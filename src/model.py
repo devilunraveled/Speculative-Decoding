63856fca63f327f0b955119a832191c41028e13b
Hardik Sharma <hardiksharmah73@gmail.com>
2024-10-21 00:27:19 +0530

    possible model change

--- src/model.py
+++ src/model.py
@@ -1,1 +1,12 @@
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
 
+model = OllamaLLM(model = "llama3.1:8b")
+
+def getResponse(query):
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "You are a helpful assistant."),
+        ("user", query),
+        ])
+    output = model.invoke({"input" : prompt})
+    print(output.logits)
