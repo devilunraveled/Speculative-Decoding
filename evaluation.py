908c744e099998292c3378a81fa23b6f07c68fc1
Hardik Sharma <hardiksharmah73@gmail.com>
2024-11-24 02:16:56 +0530

    git commit -m ' git commit -m'

--- evaluation.py
+++ evaluation.py
@@ -6,11 +6,13 @@ import pprint
 #### SQUAD Evaluation.
 def squadEvaluation(filePath, goldAnswersFile ):
     # Load the dataframe from the pickle file.
-    data = pd.read_csv(filePath)
+    data = pd.read_pickle(filePath)
     
-    totalTimeTaken = sum(data['run_time'])/len(data['run_time'])
+    totalTimeTaken = sum(data['run_time'])
+    avgTimeTaken = totalTimeTaken / len(data['run_time'])
     peakMemoryUsage = max(data['memory_footprint'])
-    # peakGPUUtilization = max( info.max_util for info in data['information'] )
+    peakGPUUtilization = max( info.max_util for info in data['information'] )
+    totalTokensGenerated = sum(info.total_generated for info in data['information'])
 
     with open(goldAnswersFile, 'rb') as f:
         goldAnswers = pkl.load(f)
@@ -23,8 +25,6 @@ def squadEvaluation(filePath, goldAnswersFile ):
         modelResponses = data['predicted_summary'].tolist()
     predictions = []
     
-
-
     for i in range(len(modelResponses)):
         predictions.append(
             {
@@ -36,15 +36,19 @@ def squadEvaluation(filePath, goldAnswersFile ):
     # Assuming 'predictions' is a list of model outputs and 'references' is a list of gold answers
     results = metric.compute(predictions=predictions, references=goldAnswers)
     assert results is not None
-    results['run_time_per_sample'] = f"{totalTimeTaken:.2f} s"
+    results['run_time_per_sample'] = f"{avgTimeTaken:.2f} s"
     results['peak_memory_footprint'] = f"{peakMemoryUsage:.2f} MB"
-    # results['peak_gpu_utilization'] = f"{peakGPUUtilization:.2f}"
+    if totalTokensGenerated == 0 :
+        results['latency_per_token'] = 0
+    else :
+        results['latency_per_token'] = f"{totalTimeTaken/totalTokensGenerated} s"
+    results['peak_gpu_utilization'] = f"{peakGPUUtilization:.2f}"
 
     pprint.pprint(results)
 if __name__ == '__main__':
     import sys
     fileName = sys.argv[1]
     datasetName = fileName.split('_')[0]
-    filePath = f"./results/{datasetName}/{fileName}.csv"
+    filePath = f"./results/{datasetName}/{fileName}.pkl"
     goldAnswersFile = f"./results/{datasetName}/{datasetName}_gold.pkl"
     squadEvaluation(filePath, goldAnswersFile)
--- results.py
+++ results.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pprint import pprint
 
-def getInformationForSpeculativeDecoder(informationObjects):
+def getInformationForSpeculativeDecoder(informationObjects, datasetName : str ):
     totalTime = sum(info.running_time for info in informationObjects)
     timePerSample = totalTime / len(informationObjects)
     
@@ -9,11 +9,41 @@ def getInformationForSpeculativeDecoder(informationObjects):
     draftRejectedTokens = sum(info.reject for info in informationObjects)
     draftAcceptedTokens = sum(info.accept_direct + info.accept_indirect for info in informationObjects)
 
+    peakVRAM = max(info.memory_footprint for info in informationObjects)
+    
     totalGenerated = sum(info.total_generated for info in informationObjects)
     modelGenerated = totalGenerated - draftAcceptedTokens
     
-    timePerToken = totalTime / totalGenerated
+    print(f"Average Length : {sum(info.total_generated for info in informationObjects) / len(informationObjects)}")
     
+    try :
+        timePerToken = totalTime / totalGenerated
+    except ZeroDivisionError:
+        timePerToken = 0
+
+        if datasetName == "squad" :
+            goldAnswers = pd.read_pickle(f"./results/{datasetName}/{datasetName}_gold.pkl")
+            lenGoldAnswers = [max( len(answer) for answer in ground_truth['answers']['text']) for ground_truth in goldAnswers ]
+
+            totalGenerated = sum(lenGoldAnswers)
+            modelGenerated = totalGenerated - draftAcceptedTokens
+            
+            timePerToken = totalTime / totalGenerated
+        elif datasetName == "storygen" :
+            lenGoldAnswers = [80 for _ in range(len(informationObjects))]
+
+            timePerToken = totalTime / sum(lenGoldAnswers)
+        elif datasetName == "billsum" :
+            lenGoldAnswers = [100 for _ in range(len(informationObjects))]
+            timePerToken = totalTime / sum(lenGoldAnswers)
+
+
+    try :
+        token_per_main_model_pass = totalGenerated / modelGenerated
+    except ZeroDivisionError:
+        token_per_main_model_pass = 0
+
+
     return {
         'time_per_sample' : f"{timePerSample:.4f} s",
         'time_per_token' : f"{timePerToken:.4f} s",
@@ -22,25 +52,28 @@ def getInformationForSpeculativeDecoder(informationObjects):
         'draft_accepted_tokens' : draftAcceptedTokens,
         'model_generated_tokens' : modelGenerated,
         'total_generated_tokens' : totalGenerated,
-        'token_per_main_model_pass' : f"{totalGenerated/modelGenerated :.4f}",
+        'token_per_main_model_pass' : f"{token_per_main_model_pass:.4f}",
+        'peak_vram' : f"{peakVRAM:.2f} MB"
     }
 
 
 #### SQUAD Results.
-def results(filePath):
+def results(filePath, dataset):
     data = pd.read_pickle(filePath)
     if 'nested' in filePath :
         layer1Decoders = data['information'].tolist()
-        pprint(getInformationForSpeculativeDecoder(layer1Decoders))
+        print("Layer 1 Decoders : ")
+        pprint(getInformationForSpeculativeDecoder(layer1Decoders, dataset))
         layer2Decoders = [info.subDecoder_info for info in data['information'].tolist()]
-        pprint(getInformationForSpeculativeDecoder(layer2Decoders))
-    elif 'speculative' in filePath:
+        print("Layer 2 Decoders :")
+        pprint(getInformationForSpeculativeDecoder(layer2Decoders, dataset))
+    else:
         # Compute total number of draft tokens
-        pprint(getInformationForSpeculativeDecoder(data['information'].tolist()))
+        pprint(getInformationForSpeculativeDecoder(data['information'].tolist(), dataset))
 
 if __name__ == '__main__':
     import sys
     fileName = sys.argv[1]
     datasetName = fileName.split('_')[0]
     filePath = f"./results/{datasetName}/{fileName}.pkl"
-    results(filePath)
+    results(filePath, datasetName)
